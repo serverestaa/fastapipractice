0b0f931ec3239e3ba9ff0ad36826444dac7ca4cc
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-07 23:38:48 +0500

    Query and Path data validation

--- books2.py
+++ books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,22 +29,24 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=3, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=-1)
 
     model_config = {
         "json_schema_extra": {
             "example": {
                 "title": "A new book",
                 "author": "codingwithme",
-                "rating": 5
+                "rating": 5,
+                "published_date": 10
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
-    Book(2, 'FastAPI', 'codingwithroby', 'A great book!', 5),
-    Book(3, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(1, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 10),
+    Book(2, 'FastAPI', 'codingwithroby', 'A great book!', 5, 15),
+    Book(3, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 11),
 ]
 
 
@@ -52,20 +56,29 @@ async def read_all_books():
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
 
 
-@app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+@app.get("/books/rating/")
+async def read_book_by_rating(book_rating: int = Query(gt=-1, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
             books_to_return.append(book)
 
     return books_to_return
+
+
+@app.get("/books/publish/")
+async def read_book_by_date(published_date: int):
+    boots_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            boots_to_return.append(book)
+    return boots_to_return
 
 
 @app.post("/create-book")
@@ -87,7 +100,7 @@ async def update_book(book: BookRequest):
 
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
