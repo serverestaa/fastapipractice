72e4f0750d6bd9ef86ac43563cea9517126ffc01
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-07 17:30:14 +0500

    get requests

--- books.py
+++ books.py
@@ -21,10 +21,34 @@ async def read_book(book_title: str):
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+
 @app.get("/books/")
-async def read_book_by_category(category: str):
-    ans = []
+async def read_category_by_query(category: str):
+    books_to_return = []
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
-            ans.append(book)
-    return ans
+            books_to_return.append(book)
+    return books_to_return
+
+
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
