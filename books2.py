5023bc795f220484cd6af56c5f9d7a92178dfccd
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-07 22:49:08 +0500

    added more get requests

--- books2.py
+++ books2.py
@@ -51,6 +51,22 @@ async def read_all_books():
     return BOOKS
 
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating:int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
