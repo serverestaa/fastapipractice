4ac6a9851a7f1202b10163931c60136d213cc5a7
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-07 23:46:51 +0500

    Http execption added, status codes after succes added

--- books2.py
+++ books2.py
@@ -50,19 +50,20 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Item not Found")
 
 
-@app.get("/books/rating/")
+@app.get("/books/rating/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(book_rating: int = Query(gt=-1, lt=6)):
     books_to_return = []
     for book in BOOKS:
@@ -72,7 +73,7 @@ async def read_book_by_rating(book_rating: int = Query(gt=-1, lt=6)):
     return books_to_return
 
 
-@app.get("/books/publish/")
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
 async def read_book_by_date(published_date: int):
     boots_to_return = []
     for book in BOOKS:
@@ -81,7 +82,7 @@ async def read_book_by_date(published_date: int):
     return boots_to_return
 
 
-@app.post("/create-book")
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
@@ -92,16 +93,24 @@ def find_book_id(book: Book):
     return book
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
