cfea240210a673819831eac4ccd142da00bea712
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-07 22:33:50 +0500

    added data validation to post request

--- books2.py
+++ books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path, Query, HTTPException, Body
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from starlette import status
 
@@ -21,6 +21,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=3, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+
+
 BOOKS = [
     Book(1, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
     Book(2, 'FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -34,5 +42,11 @@ async def read_all_books():
 
 
 @app.post("/create-book")
-async def create_book(book_request = Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id+1
+    return book
